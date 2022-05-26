a7a7a7b068309bbec70b8cc169127426013656f0
LeeRJennings <leerjennings@outlook.com>
2022-05-26 13:24:10 -0500

    phase 1

---
+++ TeamMember.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PlanYourHeist
+{
+    public class TeamMember
+    {
+        public string Name {get; set;}
+        public int SkillLevel {get; set;}
+        public decimal CourageFactor {get; set;}
+
+        public TeamMember(string name, int skillLevel, decimal courageFactor)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            CourageFactor = courageFactor;
+        }
+    }
+}
